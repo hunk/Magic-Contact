24c7cfcef4de76b08e8e58ef86ea3f2644dcb1b8
hunk <ing.edgar@gmail.com>
2010-02-23 01:10:46 -0600

    add sanitize in mail.php

--- contactable/mail.php
+++ contactable/mail.php
@@ -1,12 +1,30 @@
 <?php
   if( file_exists('../../../../wp-load.php') ) {
   	require_once('../../../../wp-load.php');
+  	
+  	//funtion clean script/html
+  	
+  	function cleanInput($input) {
+
+    $search = array(
+        '@<script[^>]*?>.*?</script>@si',   // Strip out javascript
+        '@<[\/\!]*?[^<>]*?>@si',            // Strip out HTML tags
+        '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
+        '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments
+    );
+
+        $output = preg_replace($search, '', $input);
+        return trim($output);
+    }
+    
+  	
 	  //declare our assets 
-	  $name = stripcslashes($_POST['name']);
-	  $emailAddr = stripcslashes($_POST['email']);
-	  $comment = stripcslashes($_POST['comment']);
-	  $subject = stripcslashes($_POST['subject']);	
-	  $contactMessage = "Message: $comment \r \n From: $name \r \n Reply to: $emailAddr";
+	  $name       = esc_attr(cleanInput($_POST['name']));
+	  $emailAddr  = sanitize_email($_POST['email']);
+	  $comment    = esc_attr(cleanInput($_POST['comment']));
+	  $subject    = esc_attr(cleanInput($_POST['subject']));	
+	  $website    = sanitize_url($_POST['website']);
+	  $contactMessage = "Message: $comment \r \n From: $name \r \n Website: $website \r \n Reply to: $emailAddr";
 
 	  //validate the email address on the server side
 	  if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $emailAddr) ) {
